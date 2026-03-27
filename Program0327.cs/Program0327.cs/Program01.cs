365aa9c83f838a2fadc8aa0d6e92f3284d589c6d
psy0635-ctrl <psy0635@gmail.com>
2026-03-27 12:34:21 +0900

    c#_0327

---
+++ Program0327.cs/Program0327.cs/Program01.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program0327.cs
+{
+    internal class Program01
+    {
+        static void Main(string[] args)
+        {  
+            if(DateTime.Now.Hour < 11)
+            {
+                Console.WriteLine("아침 먹을 시간입니다.");
+            }
+           else if(DateTime.Now.Hour < 15)
+            {
+                 Console.WriteLine("점심 먹을 시간입니다.");
+            }
+            else
+            {    Console.WriteLine("저녁 먹을 시간입니다.");
+            }
+        }
+    }
+}
---
+++ Program0327.cs/Program0327.cs/Program02.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program0327.cs
+{
+    internal class Program01
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("숫자를 입력하세요 : ");
+
+            String s = Console.ReadLine();
+
+            int input = int.Parse(s);
+
+            int result = input % 2;
+
+            // 조건문
+            switch (result)
+            {
+                case 0:
+                    Console.WriteLine("짝수입니다.");
+                    break;
+                case 1:
+                    Console.WriteLine("홀수입니다.");
+                    break;
+            }
+
+            if (result == 0)
+            {
+                Console.WriteLine("짝수입니다.");
+            }
+            else if (result == 1)
+            {
+                Console.WriteLine("홀수입니다.");
+
+            }
+            Console.Write("이번 달은 몇 월인가요 : ");
+
+            int input = int.Parse(Console.ReadLine());
+
+            switch (input)
+            {
+                case 12:
+                case 1:
+                case 2:
+
+                    Console.WriteLine("겨울입니다.");
+                    break;
+
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("봄입니다.");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("여름입니다.");
+                    break;
+
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("가을입니다.");
+                    break;
+
+                default:
+                    Console.WriteLine("대체 어떤 행성에 살고 계신가요?");
+                    break;
+            }
+
+            Console.WriteLine("이번 달은 몇 월인가요 : ");
+
+            String s = Console.ReadLine();
+
+            int input = int.Parse(s);
+
+            if (input < 1 || input > 12)
+            {
+                Console.WriteLine("대체 어떤 행성에 살고 계신가요?");
+            }
+
+            else if (input <= 2 || input == 12)
+            {
+                Console.WriteLine("겨울입니다.");
+            }
+
+            else if (input <= 5)
+            {
+                Console.WriteLine("봄입니다.");
+            }
+            else if (input <= 8)
+            {
+                Console.WriteLine("여름입니다.");
+            }
+            else
+            {
+                Console.WriteLine("가을입니다.");
+            }
+
+            Console.WriteLine("입력:");
+
+            string line = Console.ReadLine();
+
+            if (line.Contains("안녕"))
+            {
+                Console.WriteLine(("안녕하세요...!"));
+            }
+            else
+            {
+                Console.WriteLine("^^");
+            }
+
+
+            Console.WriteLine("출력");
+            Console.WriteLine("출력");
+            Console.WriteLine("출력");
+            Console.WriteLine("출력");
+            Console.WriteLine("출력");
+
+            for (int i = 0; i < 1000; i++)   // for (초기화; 조건식; 증감식)
+            {
+                Console.WriteLine("출력");
+            }
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i); Co
+            }
+
+            int sum = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine(sum);
+
+            int[] intArray = { 52, 273, 32, 65, 103 };
+            {
+                Console.WriteLine(intArray[0]);
+                Console.WriteLine(intArray[1]);
+                Console.WriteLine(intArray[2]);
+                Console.WriteLine(intArray[3]);
+                Console.WriteLine(intArray[4]);
+            }
+
+            int[] intArray = { 52, 273, 32, 65, 103 };
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                Console.WriteLine(intArray[i]);
+            }
+
+            int[] array = new int[100];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i + 1;
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            int[] intArray = { 52, 273, 32, 65, 103 };
+            int cnt = 0;
+
+            while (cnt < intArray.Length)
+            {
+                Console.WriteLine(cnt + "번째 출력" + intArray[cnt]);
+                cnt++;
+            }
+
+            while (!Console.ReadLine().Contains("X"))
+            {
+
+            }
+            Console.WriteLine("프로그램 종료");
+
+            string[] array = { "사과", "배", "포도", "딸기", "바나나" };
+
+            foreach (string item in array)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            string[] array = { "사과", "배", "포도", "딸기", "바나나" };
+
+            foreach (string item in array)
+            {
+                Console.WriteLine(item);
+            }
+
+            string[] array = { "사과", "배", "포도", "딸기", "바나나" };
+
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            while (true)
+            {
+                Console.WriteLine("숫자를 입력하세요 (짝수입력시 종료): ");
+                int input = int.Parse(Console.ReadLine());
+                if (input % 2 == 0)
+                {
+                    break;
+                }
+            }
+
+
+
+        }
+    }
+}
