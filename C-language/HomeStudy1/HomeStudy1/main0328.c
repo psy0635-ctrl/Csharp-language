07ce05ab1e8dc382ebdd72a8fddb515cf3bc9d20
Suyong Park <psy0635@gmail.com>
2026-03-28 16:58:59 +0900

    C_lang.0328

---
+++ C-language/HomeStudy1/HomeStudy1/main0328.c
@@ -0,0 +1,124 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    //int n;
+
+    //printf("n 값 입력: ");
+    //scanf("%d", &n);
+
+    //int count = (n + 1) / 2;
+    //int sum = count * count;
+
+    //printf("홀수의 합: %d\n", sum);
+
+    //return 0;
+
+
+ //   int n;
+ //   
+	//printf("n의 값을 입력하세요: ");
+
+	//scanf("%d", &n);
+
+	//int count = n  / 2; // 짝수의 개수 계산
+	//int sum = count * (count + 1); // 짝수의 합 계산
+
+	//printf("짝수의 합: %d\n", sum);
+
+	//return 0;
+
+    
+	//int n;
+
+	//printf("n의 값을 입력하세요: ");
+
+	//scanf("%d", &n);
+ //  
+
+	//int sum = n * (n + 1) / 2;  // 전체 합 계산
+
+	// printf("%d\n", sum);
+
+	//return 0;
+
+
+	//int age = 0;
+
+	//printf("나이를 입력하세요: ");
+	//scanf("%d", &age);
+
+	//printf("당신의 나이는 %d입니다.\n", age);
+
+	//return 0;
+
+
+	//int age = 0, birthyear= 0;
+
+	//printf("당신의 나이와 출생년도를 입력하세요: ");
+	//scanf("%d %d", &age, &birthyear);
+
+	//printf("당신의 나이는 %d이고, 출생년도는 %d입니다.\n", age, birthyear);
+
+	//return 0;
+
+
+	//printf("\t\"안녕하세요\"\n\n");
+
+	//printf("탈출기법을 제대로 사용한 예\n");
+	//printf("\t10%%5 =0\n\n");
+
+	//printf("탈출기법을 제대로 사용하지 않은 예\n");
+	//printf("\t10%5 =0\n\n");
+	//
+	//return 0;
+
+
+	//문자열 입출력 예제
+
+	//char Name[20];
+
+	//printf("이름을 입력하세요: ");
+
+	//gets(Name);
+	//puts(Name);	
+
+	//return 0;
+
+
+
+	//int n;    // 사용자로부터 입력받을 정수 변수
+
+	//printf("정수를 입력하세요: ");   // 사용자에게 정수 입력을 요청하는 메시지 출력
+	//scanf("%d", &n);				// 사용자로부터 정수를 입력받아 변수 n에 저장
+	//								// %d : 정수형 데이터를 입력 받기 위한 형식 지정자
+	//								//&n : 변수 n의 메모리 주소 -> scanf는 주소를 통해 입력 받은 값을 직접 저장
+
+	////1부터 n까지 숫자를 for문으로  순서대로 출력
+
+	//for (int i = 1; i <= n; i++) {			// i는 1부터 n까지 1씩 증가하면서 반복
+
+	//	if (i == n)
+	//		printf("%d ", i);				/*현재 반복 중인 i가 마지막 숫자 n과 같은지 확인
+	//										i가 n과 같을 때, 마지막 숫자이므로 줄바꿈 없이 출력*/
+	//	else
+	//		printf("%d ", i);				// i가 n과 다를 때, 숫자 뒤에 공백을 추가하여 출력
+	//	}
+
+	//printf("\n");						// 숫자 출력이 끝난 후 줄바꿈을 추가하여 다음 출력이 새로운 줄에서 시작되도록 함
+
+	//// n 과 5 비교
+	//if (n > 5) {						// n이 5보다 크면 참(True)
+	//	printf("%d는 5보다 큰 수 입니다.\n",n);
+	//}
+	//else if (n < 5) {					//앞의 if(n > 5)가 거짓일 때만 이 조건을 검사
+	//	printf("%d이 5보다 작은 수 입니다.\n",n);
+	//}
+	//else {								// 위의 두 조건(n>5, n<5)이 모두 거짓인 경우		
+	//	printf("5와 같은 수 입니다.\n");
+	//}
+
+	//return 0;
+
+
+}
