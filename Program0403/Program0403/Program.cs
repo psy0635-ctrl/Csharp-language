7ece0a6bfefbbf1d0fa9003a2829525f9ad4a94a
psy0635-ctrl <psy0635@gmail.com>
2026-04-03 10:01:35 +0900

    0403.C#

---
+++ Program0403/Program0403/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Program0403
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string input = "Potato Tomato";
+            //Console.WriteLine(input.ToUpper());
+            //Console.WriteLine(input.ToLower());
+
+            //Console.WriteLine(input);
+
+            //string input = "감자 고구마 토마토";
+            //string[] output = input.Split(' ');
+
+            //for (int i = 0; i < output.Length; i++)
+            //{
+            //    Console.WriteLine(output[i]);
+            //}
+
+            //string input = "감자 고구마 토마토";
+            //string output = input.Replace(" ",",");
+
+            //Console.WriteLine(output);
+
+            //string[] array = {"감자", "고구마", "토마토" };
+
+            //Console.WriteLine(string.Join("-----" , array));
+
+            //string[] array = { "감자", "고구마", "토마토" };
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.WriteLine(array[i]);
+
+            //    Thread.Sleep(1000);
+
+
+            }
+        }
+    }
+}
