932fd6c8f4749200e810f9db3276e4bcca488012
psy0635-ctrl <psy0635@gmail.com>
2026-06-05 14:18:58 +0900

    C#

---
+++ WindowsForms_0605/WindowsForms_0605/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace WindowsForms_0605
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.btnBackward = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(12, 30);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(106, 60);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "연결";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnForward
+            // 
+            this.btnForward.Location = new System.Drawing.Point(51, 374);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(89, 39);
+            this.btnForward.TabIndex = 1;
+            this.btnForward.Text = "전진";
+            this.btnForward.UseVisualStyleBackColor = true;
+            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
+            // 
+            // btnBackward
+            // 
+            this.btnBackward.Location = new System.Drawing.Point(245, 374);
+            this.btnBackward.Name = "btnBackward";
+            this.btnBackward.Size = new System.Drawing.Size(97, 39);
+            this.btnBackward.TabIndex = 2;
+            this.btnBackward.Text = "후진";
+            this.btnBackward.UseVisualStyleBackColor = true;
+            this.btnBackward.Click += new System.EventHandler(this.btnBackward_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(168, 338);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "초기상태";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(42, 156);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 120);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(199, 54);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(105, 12);
+            this.lblInfo.TabIndex = 5;
+            this.lblInfo.Text = "2601110288 박수용";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // chart1
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.chart1.Legends.Add(legend6);
+            this.chart1.Location = new System.Drawing.Point(400, 80);
+            this.chart1.Name = "chart1";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series6.Legend = "Legend1";
+            series6.Name = "Series1";
+            this.chart1.Series.Add(series6);
+            this.chart1.Size = new System.Drawing.Size(350, 300);
+            this.chart1.TabIndex = 6;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 511);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBackward);
+            this.Controls.Add(this.btnForward);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.Button btnBackward;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ WindowsForms_0605/WindowsForms_0605/Form1.cs
@@ -0,0 +1,100 @@
+﻿using ACTMULTILIB_K;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsForms_0605
+{
+    public partial class Form1 : Form
+    {
+        ActEasyIF control = new ActEasyIF();
+        int graphIndex = 0; // 그래프 X축 인덱스
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string 경로 = Application.StartupPath;
+
+            // 처음엔 후진 이미지
+            pictureBox1.Image = Image.FromFile(경로 + "\\cylinderoff.png");
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+
+            // 차트 설정
+            chart1.Series[0].ChartType = SeriesChartType.Line;
+            chart1.Series[0].BorderWidth = 2;
+            chart1.Series[0].Color = Color.Blue;
+            chart1.ChartAreas[0].AxisY.Minimum = 0;
+            chart1.ChartAreas[0].AxisY.Maximum = 100;
+            chart1.ChartAreas[0].AxisY.Interval = 20;
+
+            // 연결 전엔 전진/후진 버튼 비활성화
+            btnForward.Enabled = false;
+            btnBackward.Enabled = false;
+        }
+
+        // 연결 버튼
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (control.Open() == 0)
+            {
+                MessageBox.Show("연결되었습니다.");
+                timer1.Enabled = true;
+                btnForward.Enabled = true;
+                btnBackward.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("연결 실패하였습니다.");
+            }
+        }
+
+        // 전진 버튼
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            short value = 0x01 << 1; // Y01
+            control.WriteDeviceBlock2("Y0", 1, ref value);
+        }
+
+        // 후진 버튼
+        private void btnBackward_Click(object sender, EventArgs e)
+        {
+            short value = 0x01 << 2; // Y02
+            control.WriteDeviceBlock2("Y0", 1, ref value);
+        }
+
+        // 타이머 (1초마다 센서 읽기)
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            short sensor = 0;
+            control.ReadDeviceBlock2("X0", 1, out sensor);
+
+            if ((sensor & 0x04) != 0)
+            {
+                // 전진 상태
+                label1.Text = "전진";
+                pictureBox1.Image = Image.FromFile(Application.StartupPath + "\\cylinderon.png");
+                if (chart1.Series[0].Points.Count > 50)
+                    chart1.Series[0].Points.RemoveAt(0);
+                chart1.Series[0].Points.AddXY(graphIndex, 100);
+                chart1.ChartAreas[0].RecalculateAxesScale();
+            }
+            else if ((sensor & 0x08) != 0)
+            {
+                // 후진 상태
+                label1.Text = "후진";
+                pictureBox1.Image = Image.FromFile(Application.StartupPath + "\\cylinderoff.png");
+                if (chart1.Series[0].Points.Count > 50)
+                    chart1.Series[0].Points.RemoveAt(0);
+                chart1.Series[0].Points.AddXY(graphIndex, 0);
+                chart1.ChartAreas[0].RecalculateAxesScale();
+            }
+
+            graphIndex++; // 매 틱마다 증가
+        }
+    }
+}
