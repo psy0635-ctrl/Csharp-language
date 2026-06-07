deb8203672d7725332bd3a9b3bef665ec4b983ec
Suyong Park <psy0635@gmail.com>
2026-06-07 16:32:54 +0900

    C#_Add_button

---
+++ WindowsForms_0605_02/WindowsForms_0605_02/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace WindowsForms_0605_02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnB_Forward = new System.Windows.Forms.Button();
+            this.btnB_Backward = new System.Windows.Forms.Button();
+            this.btnC_Forward = new System.Windows.Forms.Button();
+            this.btnC_Backward = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnB_Forward
+            // 
+            this.btnB_Forward.Location = new System.Drawing.Point(99, 240);
+            this.btnB_Forward.Name = "btnB_Forward";
+            this.btnB_Forward.Size = new System.Drawing.Size(75, 23);
+            this.btnB_Forward.TabIndex = 0;
+            this.btnB_Forward.Text = "B 전진";
+            this.btnB_Forward.UseVisualStyleBackColor = true;
+            this.btnB_Forward.Click += new System.EventHandler(this.btnB_Forward_Click);
+            // 
+            // btnB_Backward
+            // 
+            this.btnB_Backward.Location = new System.Drawing.Point(237, 240);
+            this.btnB_Backward.Name = "btnB_Backward";
+            this.btnB_Backward.Size = new System.Drawing.Size(75, 23);
+            this.btnB_Backward.TabIndex = 1;
+            this.btnB_Backward.Text = "B 후진";
+            this.btnB_Backward.UseVisualStyleBackColor = true;
+            this.btnB_Backward.Click += new System.EventHandler(this.btnB_Backward_Click);
+            // 
+            // btnC_Forward
+            // 
+            this.btnC_Forward.Location = new System.Drawing.Point(99, 309);
+            this.btnC_Forward.Name = "btnC_Forward";
+            this.btnC_Forward.Size = new System.Drawing.Size(75, 23);
+            this.btnC_Forward.TabIndex = 2;
+            this.btnC_Forward.Text = "C 전진";
+            this.btnC_Forward.UseVisualStyleBackColor = true;
+            this.btnC_Forward.Click += new System.EventHandler(this.btnC_Forward_Click);
+            // 
+            // btnC_Backward
+            // 
+            this.btnC_Backward.Location = new System.Drawing.Point(237, 309);
+            this.btnC_Backward.Name = "btnC_Backward";
+            this.btnC_Backward.Size = new System.Drawing.Size(75, 23);
+            this.btnC_Backward.TabIndex = 3;
+            this.btnC_Backward.Text = "C 후진";
+            this.btnC_Backward.UseVisualStyleBackColor = true;
+            this.btnC_Backward.Click += new System.EventHandler(this.btnC_Backward_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnC_Backward);
+            this.Controls.Add(this.btnC_Forward);
+            this.Controls.Add(this.btnB_Backward);
+            this.Controls.Add(this.btnB_Forward);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnB_Forward;
+        private System.Windows.Forms.Button btnB_Backward;
+        private System.Windows.Forms.Button btnC_Forward;
+        private System.Windows.Forms.Button btnC_Backward;
+    }
+}
+
---
+++ WindowsForms_0605_02/WindowsForms_0605_02/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ActMultiLib;
+
+namespace WindowsForms_0605_02
+{
+    public partial class Form1 : Form
+    {
+        // ★ PLC 통신 객체 선언
+        ActEasyIF plc = new ActEasyIF();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // PLC 통신 설정
+            plc.ActLogicalStationNumber = 1; // 시뮬레이터 국번 (보통 1)
+
+            int ret = plc.Open(); // 연결 시작
+            if (ret != 0)
+            {
+                MessageBox.Show($"PLC 연결 실패! 오류코드: {ret}");
+            }
+            else
+            {
+                MessageBox.Show("PLC 연결 성공!");
+            }
+        }
+
+        // =============================================
+        //  PLC에 명령 보내는 핵심 함수 (공통으로 사용)
+        // =============================================
+        // deviceName : "Y01", "Y02" 등 Y 디바이스 주소
+        // value      : 1 = ON(동작), 0 = OFF(정지)
+        private void PLC쓰기(string deviceName, int value)
+        {
+            int[] data = new int[] { value }; // 보낼 데이터 배열
+            int ret = plc.WriteDeviceBlock2(deviceName, 1, ref data[0]);
+
+            if (ret != 0)
+            {
+                MessageBox.Show($"{deviceName} 쓰기 실패! 오류코드: {ret}");
+            }
+        }
+
+        //  B 실린더 전진 버튼 (Y01 = ON)
+        private void btnB_Forward_Click(object sender, EventArgs e)
+        {
+            PLC쓰기("Y01", 1); // B 전진 ON
+            PLC쓰기("Y02", 0); // B 후진 OFF (동시 작동 방지)
+        }
+
+        //  B 실린더 후진 버튼 (Y02 = ON)
+        private void btnB_Backward_Click(object sender, EventArgs e)
+        {
+            PLC쓰기("Y02", 1); // B 후진 ON
+            PLC쓰기("Y01", 0); // B 전진 OFF (동시 작동 방지)
+        }
+
+        //  C 실린더 전진 버튼 (Y03 = ON)
+        private void btnC_Forward_Click(object sender, EventArgs e)
+        {
+            PLC쓰기("Y03", 1); // C 전진 ON
+            PLC쓰기("Y04", 0); // C 후진 OFF (동시 작동 방지)
+        }
+
+        //  C 실린더 후진 버튼 (Y04 = ON)
+        private void btnC_Backward_Click(object sender, EventArgs e)
+        {
+            PLC쓰기("Y04", 1); // C 후진 ON
+            PLC쓰기("Y03", 0); // C 전진 OFF (동시 작동 방지)
+        }
+
+        //  폼 닫힐 때 PLC 연결 해제
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            plc.Close();
+        }
+    }
+}
